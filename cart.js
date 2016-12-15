89b78fadf99b51d71779bf47a4fe5059cc9139ca
hugh88 <7hgallagly@stpatrick.org>
2016-12-15 10:52:29 -0600

    cart
    finished cart but i still need to make sure that i have everything down.

--- cart.js
+++ cart.js
@@ -10,6 +10,19 @@ let q = itemQuantity.push();
 let i = itemList.push();
 }
 totalCart(){
-  let total = 0
-  
+  let total = 0;
+  for(var c=0; c < this.itemList[].length;c++)
+    total = total+this.itemlist[c].price+this.itemQuantity[c]
+
+return total;
+}
+subCart(days){
+    let subCart = new Cart();
+      for(var c=0; c < this.itemList[].length;c++)
+        if(this.itemList[c].shipping == days){
+          subCart.itemList.push(this.itemList[c]);
+          subCart.itemQuantity.push(this.itemQuantity[c]);
+        }
+return subCart;
+}
 }
