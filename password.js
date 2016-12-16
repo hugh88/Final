58155efd7294225544e183bea4c39921abbb45db
hugh88 <7hgallagly@stpatrick.org>
2016-12-16 10:54:39 -0600

    finished cart and worked on password

--- password.js
+++ password.js
@@ -17,4 +17,18 @@ validPublicKey(){
 
 
   //Static function below this comment.
-}
+  static makePrivateKey(){
+    let key = "";
+    let limit = 14;
+    while(var x=0;x<limit;x++){
+      if(x==4 || x==9){
+        key += "-";
+      }
+      else{
+        var number = Math.random()*10;
+        number = Math.floor(number);
+        key += number;
+      }
+    }
+    return key;
+  }
